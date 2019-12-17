3dfe19736fe3e55474d5b594c14052be70551710
Tintin <tini_subha@yahoo.co.in>
2019-12-18 02:02:02 +0530

    Forecast output changed2

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The highest temperature was '+ body.daily.data[0].temperatureMax +' and the lowest was '+ body.daily.data[0].temperatureMin)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The highest temperature was '+ body.daily.data[0].temperatureHigh +' and the lowest was '+ body.daily.data[0].temperatureLow)
         }
     })
 }
